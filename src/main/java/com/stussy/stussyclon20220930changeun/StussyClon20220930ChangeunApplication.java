2624f548d5d10d440be70afad1e9aeb9cba8b424
eun091696 <eun091696@gmail.com>
2022-09-30 17:55:21 +0900

    Stussy / Account화면 구현 Day01 (22.09.30)

---
+++ src/main/java/com/stussy/stussyclon20220930changeun/StussyClon20220930ChangeunApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclon20220930changeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClon20220930ChangeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClon20220930ChangeunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stussy/stussyclon20220930changeun/StussyClon20220930ChangeunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclon20220930changeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClon20220930ChangeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
